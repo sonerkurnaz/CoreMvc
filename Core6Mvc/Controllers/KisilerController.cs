d6440ff6b608dd91a772e19ebd7b8bf43c820db8
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-06-20 12:31:29 +0300

    Add project files.

---
+++ Core6Mvc/Controllers/KisilerController.cs
@@ -0,0 +1,20 @@
+﻿using Core6Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core6Mvc.Controllers
+{
+    public class KisilerController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Kisi> kisiler = new List<Kisi>
+            {
+                new Kisi{Ad="Ali",Soyad="Kaya",Gsm="532"},
+                new Kisi{Ad="Ayse",Soyad="Yilmaz",Gsm="532"},
+                new Kisi{Ad="Veli",Soyad="Gexer",Gsm="532"},
+                new Kisi{Ad="Ali",Soyad="Kaya",Gsm="532"}
+            };
+            return View(kisiler);
+        }
+    }
+}
---
+++ Core6Mvc/Models/Kisi.cs
@@ -0,0 +1,10 @@
+﻿namespace Core6Mvc.Models
+{
+    public class Kisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Gsm { get; set; }
+
+    }
+}
