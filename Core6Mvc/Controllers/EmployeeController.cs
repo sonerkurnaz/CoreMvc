e206c6d1dfa23b8d9cc48f65e0df99044c5ec34a
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-06-22 01:37:53 +0300

    son degisiklik

---
+++ Core6Mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Entities;
+
+namespace Core6Mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly NorthwindContext context;
+
+        public EmployeeController(NorthwindContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
