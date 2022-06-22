2ea913068c1d74f60bbdb48a268b82dca8e5b3c2
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-06-22 11:03:22 +0300

    employee liste

--- Core6Mvc/Controllers/EmployeeController.cs
+++ Core6Mvc/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core6Mvc.Models.DTO.Employees;
+using Microsoft.AspNetCore.Mvc;
 using Northwind.Entities;
 
 namespace Core6Mvc.Controllers
@@ -11,9 +12,31 @@ namespace Core6Mvc.Controllers
         {
             this.context = context;
         }
-
         public IActionResult Index()
         {
+            //
+            List<EmployeeListDto> listDtos = new List<EmployeeListDto>();
+
+            //database'den calısanları cekiyoruz
+            var EmployeeList = context.Employees.ToList();
+
+            //database den gelen verileri Employeelistdto ya teker teker atıyoruz
+            foreach (var employee in EmployeeList)
+            {
+                EmployeeListDto listDto = new();
+                listDto.Id = employee.EmployeeId;
+                listDto.FirstName = employee.FirstName;
+                listDto.LastName = employee.LastName;
+                listDto.HireDate = employee.HireDate;
+                listDto.Country = employee.Country;
+                listDto.City = employee.City;
+                listDto.HomePhone = employee.HomePhone;
+
+                //Listeye ekliyoruz..
+                listDtos.Add(listDto);
+
+            }
+
             return View();
         }
     }
---
+++ Core6Mvc/Models/DTO/Employees/EmployeeListDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Core6Mvc.Models.DTO.Employees
+{
+    public class EmployeeListDto
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? HireDate { get; set; }
+        public string? Title { get; set; }
+        public string? Country { get; set; }
+        public string? City { get; set; }
+        public string? HomePhone { get; set; }
+
+    }
+}
