3b3c3c09e04fe1b94869538322732252f29a41c9
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-06-23 10:59:15 +0300

    Implemented

--- Northwind.DAL/Abstract/IRepositoryBase.cs
+++ Northwind.DAL/Abstract/IRepositoryBase.cs
@@ -7,6 +7,7 @@ namespace Northwind.DAL.Abstract
         int Add(T input);
         int Update(T input);
         int Delete(T input);
+        T Find(int id);
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         List<T> GetAllInclude(Expression<Func<T, bool>> filter = null,
                        params Expression<Func<T, object>>[] include);
---
+++ Northwind.DAL/Concrete/RepositoryBase.cs
@@ -0,0 +1,46 @@
+﻿using Northwind.DAL.Abstract;
+using Northwind.Entities;
+using System.Linq.Expressions;
+
+namespace Northwind.DAL.Concrete
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class, new()
+    {
+        NorthwindContext db;
+        public RepositoryBase()
+        {
+            db = new NorthwindContext();
+        }
+        public int Add(T input)
+        {
+            db.Set<T>().Add(input);
+            return db.SaveChanges();
+        }
+        public int Update(T input)
+        {
+            db.Set<T>().Update(input);
+            return db.SaveChanges();
+        }
+
+        public int Delete(T input)
+        {
+            db.Set<T>().Remove(input);
+            return db.SaveChanges();
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAllInclude(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] include)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
